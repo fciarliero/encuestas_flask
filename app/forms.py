fbd1b938f33da91b40790368ca9134feb575f512
fciarliero <fciarliero@dc.uba.ar>
2020-11-18 19:28:21 -0300

    linkea views de login y signup con flask_login. funciona la generacion de usuarios nuevos y logueo de existentes

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, FieldList, FormField, Form
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from app.models import User
 
 
 class QuestionForm(Form):
@@ -19,8 +20,23 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is None or not user.check_password(self.password.data):
+            raise ValidationError('Wrong username or password.')
+        else:
+            pass
+
+
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    repat_pass = PasswordField('Repeat Password', validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
+    repeat_pass = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request, session
-from app import app
+from werkzeug.urls import url_parse
+
+from app import app, db
 from app.forms import QuestionForm, SurveyForm, LoginForm, SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 
 @app.route('/')
@@ -33,20 +37,47 @@ def poll():
     return render_template('survey.html', form = form2)
 
 
+@app.route('/new_poll', methods=['GET', 'POST'])
+@login_required
+def new_poll():
+    return "aca se va a generar una encuesta"
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    session.pop('user_id', None)
-    if request.method == 'POST' and form.validate_on_submit():
-        session['user_id'] = form.user.data
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
-    return render_template('login.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        #esta parte hace que si te logueas despues de ir a un url con login required, vuelvas a el
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    return render_template('login.html', title='Log In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = SignUpForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        session['user_id'] = form.user.data
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now a registered!')
         return redirect(url_for('login'))
-    return render_template('signup.html', form=form)
+    return render_template('signup.html', title='Register', form=form)
