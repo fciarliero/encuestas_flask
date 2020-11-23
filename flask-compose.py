5383d7717d0bf68f5e220616c2042bc33076835c
fciarliero <fciarliero@dc.uba.ar>
2020-11-22 22:20:05 -0300

    cambios para que arranque docker

--- flask-compose.py
+++ flask-compose.py
@@ -1,11 +1,10 @@
 from app import app
 '''
 from flask import Flask
-
+#from app import app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secreto'
-
+'''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-    '''
+    app.run(host='0.0.0.0', port='5050')
